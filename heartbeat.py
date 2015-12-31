260981f946de12869f2c005fcfd833e7af7b783a
mjsrs <mjsrs@sapo.pt>
2015-12-31 01:09:17 +0000

    changed url which gets external ip

--- heartbeat.py
+++ heartbeat.py
@@ -4,11 +4,11 @@ import datetime
 try:
     import requests
 except ImportError:
-    sys.exit("python requests not installed. run 'sudo easy_install install requests'")
+    sys.exit("python requests not installed. run 'sudo easy_install requests'")
 try:
     from firebase import firebase
 except ImportError:
-    sys.exit("python-firebase not installed. run 'sudo easy_install install python-firebase'")
+    sys.exit("python-firebase not installed. run 'sudo easy_install python-firebase'")
 try:
     config = __import__('config')
 except ImportError:
@@ -27,7 +27,7 @@ def main():
     # get local date and time
     output['local_datetime'] = datetime.datetime.now()
     try:
-        r = requests.get('http://myexternalip.com/raw')
+        r = requests.get('https://api.ipify.org')
         output['global_ip'] = r.text
     except requests.exceptions.RequestException as e:
         message = e
